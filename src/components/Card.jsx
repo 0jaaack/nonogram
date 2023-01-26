9fe897f358d76f47778de9ec69920408ef891f8f
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-01-27 04:29:17 +0900

    [Chore] add card solve styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,13 +7,17 @@ class Card extends React.Component {
   }
 
   render() {
+    const cardSolvedStyle = this.props.puzzle.isPuzzleDone
+      ? "from-emerald-500 to-stone-400"
+      : "from-gray-700 to-cyan-600";
+
     return (
       <div
-        className="flex items-center justify-center w-48 h-60 bg-gradient-to-tr from-gray-900 to-cyan-800 rounded-xl cursor-pointer"
+        className={`flex flex-none items-center justify-center w-48 h-60 bg-gradient-to-tr rounded-xl cursor-pointer ${cardSolvedStyle}`}
         style={{ boxShadow: "rgba(155, 155, 144, 0.2) 0px 7px 29px 0px" }}
-        onClick={() => this.props.navigate(`/puzzles/${this.props.value.title}`)}
+        onClick={() => this.props.navigate(`/puzzles/${this.props.puzzle.title}`)}
       >
-        <h3 className="text-3xl italic uppercase font-bold text-center">{this.props.value.title}</h3>
+        <h3 className="text-2xl italic uppercase font-bold text-center">{this.props.puzzle.title}</h3>
       </div>
     );
   }
