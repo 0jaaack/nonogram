4976dba0b101327e3931a0f66e0ac57b1663ab9b
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-01-27 04:35:39 +0900

    [Feature] add drag event stability / add puzzles

--- src/features/puzzle/reducers.js
+++ src/features/puzzle/reducers.js
@@ -14,25 +14,135 @@ const toLineCount = (line) => {
 
 const initialState = {
   puzzles: {
-    "talk": {
+    talk: {
       title: "talk",
-      slug: "talk",
       size: "size1",
-      cellSize: 6,
       column: toLineCount([[7], [2, 2, 1], [4, 1], [1], [5], [1, 1], [1, 1, 1, 1], [1, 1, 2], [1, 2], [5]]),
       row: toLineCount([[6], [1, 1], [1, 2, 1], [2, 1, 1], [3, 3, 2], [1, 1, 2], [4, 2], [3], [1], [3]]),
       board: Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => null)),
       isPuzzleDone: false,
     },
+    rabbit: {
+      title: "rabbit",
+      size: "size1",
+      column: toLineCount([[4], [4, 2], [1, 1, 2], [4, 1], [1, 1], [1, 2], [1, 2, 3], [3, 1], [2], [4]]),
+      row: toLineCount([[2, 3], [1, 2, 1], [1, 1, 2], [1, 1, 2], [2, 1], [1, 1, 1, 3], [1, 1, 2], [2, 1, 1], [3, 1], [1, 1]]),
+      board: Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => null)),
+      isPuzzleDone: false,
+    },
+    fencing: {
+      title: "fencing",
+      size: "size1",
+      column: toLineCount([[2, 2], [1, 1, 2], [5], [7], [2, 1, 1], [1, 1], [1, 1, 4], [1, 1], [1], [1]]),
+      row: toLineCount([[2, 2], [1, 2], [1, 1, 1], [4, 3], [2, 1], [2], [5], [2, 1], [2, 1], [1, 2]]),
+      board: Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => null)),
+      isPuzzleDone: false,
+    },
+    mushroom: {
+      title: "mushroom",
+      size: "size1",
+      column: toLineCount([[2], [1, 2], [5, 2], [7, 1], [1, 1, 1, 1], [1, 2, 1, 1], [1, 6, 1], [1, 3, 1], [4], [2]]),
+      row: toLineCount([[4], [2, 1], [6, 1], [1, 2, 5], [4, 4], [8], [1, 1], [1, 1], [1, 1], [4]]),
+      board: Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => null)),
+      isPuzzleDone: false,
+    },
+    pinocchio: {
+      title: "pinocchio",
+      size: "size1",
+      column: toLineCount([[1], [6, 2], [3, 3], [2, 1], [2, 2, 1], [3, 3], [1, 3, 1], [1, 1], [1], [1]]),
+      row: toLineCount([[4], [8], [2, 1], [1, 1, 1], [1, 1, 4], [1, 1], [2, 1], [4], [2, 1], [1, 1]]),
+      board: Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => null)),
+      isPuzzleDone: false,
+    },
+    butterfly: {
+      title: "butterfly",
+      size: "size2",
+      board: Array.from({ length: 15 }, () => Array.from({ length: 15 }, () => null)),
+      row: toLineCount([[2], [1], [5, 1, 2], [1, 1, 1, 1], [1, 2, 1, 4], [1, 1, 1, 1, 1], [1, 3, 1], [1, 2, 1], [3, 2, 2], [2, 1, 3], [1, 1, 1], [2, 1, 1, 1], [1, 1, 1], [4, 4], []]),
+      column: toLineCount([[7], [1, 2], [1, 2, 3], [1, 2, 1], [3, 1], [1, 1], [2, 2], [4, 4], [1, 5, 3], [2, 1], [1, 1, 2, 1], [1, 1, 3], [1, 1], [1, 1], [5]]),
+      isPuzzleDone: false,
+    },
+    "hula-hoop": {
+      title: "hula-hoop",
+      size: "size2",
+      board: Array.from({ length: 15 }, () => Array.from({ length: 15 }, () => null)),
+      row: toLineCount([[4], [2, 1, 1, 2], [1, 1, 2, 1, 1, 1, 1], [1, 1, 1, 2, 1], [1, 1, 4, 1], [1, 1], [2, 3], [3, 1, 2], [2, 1, 1, 1, 1], [1, 2, 2, 1], [2, 1, 4, 1, 2], [3, 3], [10], [1, 2, 1], [1, 1, 1, 1]]),
+      column: toLineCount([[4, 3], [1, 1, 2, 2], [2, 2, 2], [1, 9], [3, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 2], [1, 1, 1, 1, 2], [1, 1, 1, 1, 1], [3, 1, 1], [1, 9], [1, 2, 2], [1, 1, 2, 2], [3, 4], []]),
+      isPuzzleDone: false,
+    },
+    mantis: {
+      title: "mantis",
+      size: "size2",
+      board: Array.from({ length: 15 }, () => Array.from({ length: 15 }, () => null)),
+      row: toLineCount([[1, 1, 6, 1, 2], [4, 4, 5], [5, 6], [3, 2, 4], [3, 1, 1, 4], [2, 4, 3], [2, 3], [1, 2, 3, 1], [2, 2, 2, 1], [2, 2], [3, 1, 2], [6, 4, 1], [5, 6], [1, 3, 4, 1], [1, 1, 2, 1]]),
+      column: toLineCount([[5, 3], [4, 2, 2, 1], [6, 2, 3], [2, 2, 6], [1, 1, 1, 2, 4], [2, 1, 1, 2], [2, 1, 1, 1], [2, 1, 1, 1], [2, 1, 1, 2], [1, 1, 1, 3, 3], [2, 4, 4], [7, 5], [5, 2, 2], [5, 3, 1, 1], [5, 3]]),
+      isPuzzleDone: false,
+    },
+    coconut: {
+      title: "coconut",
+      size: "size2",
+      board: Array.from({ length: 15 }, () => Array.from({ length: 15 }, () => null)),
+      row: toLineCount([[1, 1, 2, 3], [2, 1, 2, 1, 1], [2, 1, 5, 2], [1, 1, 1, 6], [1, 2, 2, 1], [1, 4], [3, 1], [2, 2, 1], [1, 2, 2, 2], [4, 2, 2, 1], [1, 2, 1, 2, 2, 1], [4, 4, 5], [5, 2, 6], [6, 7], [15]]),
+      column: toLineCount([[1, 1, 5], [2, 1, 2, 4], [2, 1, 6], [1, 1, 8], [1, 2, 3], [1, 1, 2, 2], [1, 1, 1, 1, 2, 1], [3, 1, 4, 1], [2, 1, 2, 2], [2, 2, 3], [14], [1, 12], [1, 2, 1, 4], [1, 2, 2, 5], [1, 2, 1, 2, 4]]),
+      isPuzzleDone: false,
+    },
+    bear: {
+      title: "bear",
+      size: "size2",
+      board: Array.from({ length: 15 }, () => Array.from({ length: 15 }, () => null)),
+      row: toLineCount([[3], [7, 1], [5, 2, 1, 1, 1], [2, 2, 1, 2], [1, 5], [4, 1, 2], [2, 3, 1, 1, 2], [3, 1, 4], [5, 3, 1], [4, 2], [2, 1, 3], [2, 1, 2, 2, 1], [1, 1, 3, 2], [1, 1, 2, 1], [2, 1, 3]]),
+      column: toLineCount([[5], [2, 3], [1, 1, 1], [1, 1, 2, 2, 1], [2, 1, 1, 1, 2], [1, 1, 1, 1, 1], [3, 1, 1, 4], [2, 2], [1, 1, 3], [2, 2, 2, 2, 1], [2, 2, 4, 2], [1, 1, 5, 5], [1, 3, 1, 3, 1], [4, 2, 1, 3], [1, 1, 1, 2]]),
+      isPuzzleDone: false,
+    },
+    peanut: {
+      title: "peanut",
+      size: "size3",
+      board: Array.from({ length: 20 }, () => Array.from({ length: 20 }, () => null)),
+      row: toLineCount([[1], [3], [1, 1, 2], [1, 3], [1, 1, 1, 2], [1, 1], [1, 2, 2, 1], [2, 2, 1, 2, 1], [3, 1, 1, 1, 1], [4, 1, 3, 1, 2], [3, 3, 3, 2], [2, 2], [2, 2, 3], [3, 3, 4], [4, 4, 3], [4, 5, 3], [5, 6, 3], [4, 6, 4], [2, 6, 5], [20]]),
+      column: toLineCount([[16], [13], [3, 5, 1], [1, 4, 1], [1, 1], [1], [2], [3], [3, 5], [2, 1, 8], [1, 1, 8], [1, 1, 2, 7], [1, 3, 4, 1], [1, 1, 2, 1, 1], [1, 1, 1], [1, 2, 1, 2], [1, 3, 1, 3], [3, 8], [3, 11], [1, 17]]),
+      isPuzzleDone: false,
+    },
+    puppy: {
+      title: "puppy",
+      size: "size3",
+      board: Array.from({ length: 20 }, () => Array.from({ length: 20 }, () => null)),
+      row: toLineCount([[11], [2, 1, 4], [2, 1, 1, 2], [1, 1, 1, 1], [1, 2, 1, 1, 2, 2], [3, 1, 1, 3], [1, 1], [3, 2], [4, 1, 1, 2], [1, 1, 2, 2, 1, 1], [8, 2, 1, 1], [1, 3, 1], [2, 1, 2, 2, 1], [1, 4, 2, 1], [2, 1, 2, 1, 1], [1, 3, 3, 1], [1, 1, 2, 1, 1], [1, 2, 5, 2], [6, 3], [1, 5]]),
+      column: toLineCount([[], [], [], [3, 5], [2, 1, 3, 3, 1], [2, 2, 2, 3, 2], [1, 8, 2], [1, 1, 2, 1, 4], [2, 2, 1, 3, 2], [1, 1, 2, 2, 1], [1, 1, 1, 2, 1], [1, 2, 2, 1, 1], [1, 2, 1, 1, 2, 1, 1], [1, 1, 2, 1, 1], [5, 2, 6], [2, 2, 5, 1], [1, 3, 2, 2], [2, 2, 10], [3, 1], [4]]),
+      isPuzzleDone: false,
+    },
+    shark: {
+      title: "shark",
+      size: "size3",
+      board: Array.from({ length: 20 }, () => Array.from({ length: 20 }, () => null)),
+      row: toLineCount([[2], [3], [3, 4], [1, 2, 5, 2], [13, 2], [10], [1, 12], [1, 1, 14], [1, 15], [10, 2, 2], [4, 5, 2, 3], [4, 1, 3, 2, 2], [7, 2, 8], [8, 1, 8], [8, 2, 4, 3], [8, 2, 3], [4, 3, 1, 3], [5], [4], [2]]),
+      column: toLineCount([[1, 3], [1, 1, 4], [1, 1, 1, 5], [2, 1, 7], [2, 8], [1, 9], [1, 4, 5], [1, 3, 5], [1, 5, 1], [1, 5, 1, 1, 1], [7, 5], [9, 1], [13], [9, 4], [9, 1, 3], [15, 4], [6, 3, 3], [1, 4, 1, 7], [16], [14]]),
+      isPuzzleDone: false,
+    },
   },
   sizes: {
     byId: {
-      size1: "10 X 10",
+      size1: {
+        x: 10,
+        y: 10,
+        cell: 10,
+      },
+      size2: {
+        x: 15,
+        y: 15,
+        cell: 8,
+      },
+      size3: {
+        x: 20,
+        y: 20,
+        cell: 7,
+      },
     },
-    allIds: ["size1"],
+    allIds: ["size1", "size2", "size3"],
   },
   puzzlesBySize: {
-    size1: ["talk"],
+    size1: ["talk", "rabbit", "fencing", "mushroom", "pinocchio"],
+    size2: ["butterfly", "hula-hoop", "mantis", "coconut", "bear"],
+    size3: ["peanut", "puppy", "shark"],
   },
 };
 
@@ -48,8 +158,13 @@ function puzzleReducer(state = initialState, action) {
       const boardRow = checkingBoard[x].slice();
       const puzzleRow = checkingPuzzle.row[x].slice();
       const puzzleRowCounts = puzzleRow.map((lineCount) => lineCount.count);
+      const rowLineCounts = getLineCount(boardRow);
 
-      if (puzzleRowCounts.join() === getLineCount(boardRow).join()) {
+      if (rowLineCounts.length > puzzleRowCounts.length) {
+        puzzleRow.forEach((lineCount) => {
+          lineCount.isDone = false;
+        });
+      } else if (puzzleRowCounts.join() === rowLineCounts.join()) {
         puzzleRow.forEach((lineCount) => {
           lineCount.isDone = true;
         });
@@ -59,7 +174,7 @@ function puzzleReducer(state = initialState, action) {
 
         for (const [countIdx, lineCount] of puzzleRow.entries()) {
           lineCount.isDone = lineCount.count === rowStartLineCounts[countIdx];
-        };
+        }
         for (const [countIdx, count] of rowEndLineCounts.entries()) {
           const puzzleRowIdx = puzzleRow.length - countIdx - 1;
 
@@ -69,14 +184,19 @@ function puzzleReducer(state = initialState, action) {
           }
 
           break;
-        };
+        }
       }
 
       const boardColumn = checkingBoard.map((row) => row[y]);
       const puzzleColumn = checkingPuzzle.column[y].slice();
       const puzzleColumnCounts = puzzleColumn.map((lineCount) => lineCount.count);
+      const columnLineCounts = getLineCount(boardColumn);
 
-      if (puzzleColumnCounts.join() === getLineCount(boardColumn).join()) {
+      if (columnLineCounts.length > puzzleColumnCounts.length) {
+        puzzleColumn.forEach((lineCount) => {
+          lineCount.isDone = false;
+        });
+      } else if (puzzleColumnCounts.join() === columnLineCounts.join()) {
         puzzleColumn.forEach((lineCount) => {
           lineCount.isDone = true;
         });
@@ -86,7 +206,7 @@ function puzzleReducer(state = initialState, action) {
 
         for (const [countIdx, lineCount] of puzzleColumn.entries()) {
           lineCount.isDone = lineCount.count === columnStartLineCounts[countIdx];
-        };
+        }
         for (const [countIdx, count] of columnEndLineCounts.entries()) {
           const puzzleColumnIdx = puzzleColumn.length - countIdx - 1;
 
@@ -96,7 +216,7 @@ function puzzleReducer(state = initialState, action) {
           }
 
           break;
-        };
+        }
       }
 
       checkingPuzzle.row[x] = puzzleRow;
@@ -119,6 +239,16 @@ function puzzleReducer(state = initialState, action) {
             if (!count.isDone) {
               result = false;
               break;
+            }
+          }
+        }
+      }
+
+      if (result) {
+        for (const row of checkingBoard) {
+          for (const [idx, cell] of row.entries()) {
+            if (cell === PUZZLE.CELL_TYPE.EMPTY || !cell) {
+              row[idx] = PUZZLE.CELL_TYPE.MARK;
             }
           }
         }
